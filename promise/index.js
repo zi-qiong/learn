2c979e0710ba1a0077e05a52899e0f8708fa3df9
谢子琼 <ziqiong.xie@smartx.com>
2020-11-19 18:41:15 +0800

    promise

---
+++ promise/index.js
@@ -0,0 +1,38 @@
+const isFunction = fn => typeof fn === "function"
+
+class Promise {
+    constructor(handle) {
+        if (!isFunction(handle)) {
+            throw new Error("Promise must accept a function as a parameser")
+        }
+
+        this._status = "pending"
+        this._value = undefined
+
+        try {
+            handle(this._resolve.bind(this), this._reject.bind(this)) 
+        } catch (err) {
+            this._reject(err)
+        }
+    }
+
+    _resolve(val) {
+        if(this._status !== "pending") return
+
+        this._status = "fulfilled"
+        this._value = val
+    }
+
+    _resolve(err) {
+        if(this._status !== "pending") return
+
+        this._status = "rejected"
+        this._value = err
+    }
+}
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("resolve")
+    }, 1000)
+})
